861faa2597a142a2abb4417867a1e4e69cd9366d
sopnaparvin1122 <islamanisha860@gmail.com>
2023-09-25 10:26:49 +0600

    forloop done

--- forloop.js
+++ forloop.js
@@ -1,11 +1,20 @@
-// let i = 0;
-// for (i < 10 ; i++ ) {
-//     console.log();
-// }
+// // let i = 0;
+// // for (i < 10 ; i++ ) {
+// //     console.log();
+// // }
 
 for ( i = 1 ; i <= 10 ; i++ ){
     
-    console.log("2*1=" ,i * 2);
+    console.log( '2 * 1 =', i * 2);
   
 
 }
+
+
+
+// for (let i = 1; i < 10; i++) {
+//     for(n = 2 ; n < 10; i++){
+//         item = n*i
+//     console.log( `${n} * ${i} = ${item}` );
+//     }
+//   }
